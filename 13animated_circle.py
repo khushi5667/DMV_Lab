c1f70394ec1259431f8d49d133b9cdc94404f716
khushi5667 <khushikumari0983@gmail.com>
2026-04-05 14:10:39 +0530

    13animated_circle.py

---
+++ 13animated_circle.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.animation import FuncAnimation
+
+# Create figure and axis
+fig, ax = plt.subplots()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_aspect('equal')  # Keep circle round
+
+# Create a circle
+circle = Circle((0, 5), 0.5, color='blue')
+ax.add_patch(circle)
+
+# Update function for animation
+def update(frame):
+    circle.center = (frame, 5)  # Move horizontally
+    return circle,
+
+# Create animation
+ani = FuncAnimation(fig, update, frames=range(11), interval=200, blit=True)
+
+plt.show()
