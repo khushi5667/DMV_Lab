9cc857cb61085611c1b0d697ba2b87455faa2cc8
khushi5667 <khushikumari0983@gmail.com>
2026-04-05 14:22:12 +0530

    Create 2static_hist.py

---
+++ 2static_hist.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+data = [10, 12, 15, 20, 22, 25, 25, 30, 32, 35, 40]
+
+plt.hist(data, bins=5)
+plt.xlabel("Data values")
+plt.ylabel("Frequency")
+plt.title("Simple Histogram")
+plt.show()
